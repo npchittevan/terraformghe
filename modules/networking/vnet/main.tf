6ff1fe8c71867f6afff7cbffd26978a6009b5924
npchittevan <nilchittevan@gmail.com>
2025-12-05 23:24:08 +0530

    changes done for resource name

--- modules/networking/vnet/main.tf
+++ modules/networking/vnet/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm-virtual_network" "virtual-network" {
+resource "azurerm_virtual_network" "virtual-network" {
   name                = var.vnet_name
   address_space       = [var.vnet_address_prefix]
   location            = var.location
@@ -8,6 +8,6 @@ resource "azurerm_subnet" "network_subnet" {
   count               = var.vnet_subnet_count
   name                = "subnet-${count.index}"
   resource_group_name = var.resource_group_name
-  virtual_network_name= azurerm-virtual_network.virtual-network.name
+  virtual_network_name= azurerm_virtual_network.virtual-network.name
   address_prefixes    = [cidrsubnet((var.vnet_address_prefix),8,count.index)]
  }
