8692b146630c7bfddfe1803bf7e0a72be451a387
npchittevan <nilchittevan@gmail.com>
2025-12-06 18:02:36 +0530

    corrected error

--- modules/networking/vnet/output.tf
+++ modules/networking/vnet/output.tf
@@ -2,10 +2,6 @@ output "virtual_network_interface_ids" {
   value = azurerm_network_interface.network_interface[*].id
 }
 
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ipaddress[*].id.ip_address
-}
-
 output "virtual_network_id" {
   value = azurerm_virtual_network.virtual-network.id
 }
